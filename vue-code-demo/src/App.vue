4ca1eb1a8a071235b76df335db2ca4e8fd1788d5
zt <2249269086@qq.com>
2021-05-16 17:54:29 +0800

    feat: init

---
+++ vue-code-demo/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <!-- <TplDemo /> -->
+    <!-- <ConditionDemo/> -->
+    <!-- <EventDemo/> -->
+
+    <!-- <ComponentDemo/> -->
+
+    <!-- <AdvancedUse/> -->
+
+    <!-- <CartDemo/> -->
+    <!-- <computedDemo /> -->
+    <WatchDemo></WatchDemo>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// import TplDemo from './components/BaseUse/TplDemo'
+// import ConditionDemo from './components/BaseUse/ConditionDemo'
+// import EventDemo from './components/BaseUse/EventDemo'
+
+// import ComponentDemo from './components/ComponentsDemo/index'
+
+// import AdvancedUse from './components/AdvancedUse/index'
+
+// import CartDemo from './components/Cart/index'
+// import computedDemo from './components/BaseUse/ComputedDemo'
+import WatchDemo from './components/BaseUse/WatchDemo'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+    // EventDemo
+    // ComponentDemo
+    // AdvancedUse
+    // CartDemo
+    // TplDemo,
+    // computedDemo,
+    WatchDemo
+  },
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ vue-code-demo/src/components/BaseUse/ComputedDemo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!--1.直接拼接:语法过于繁琐-->
+    <h2>{{ firstName }} {{ lastName }}</h2>
+
+    <!--2.通过定义methods-->
+    <h2>{{ getFullName() }}</h2>
+    <h2>{{ getFullName() }}</h2>
+    <h2>{{ getFullName() }}</h2>
+    <h2>{{ getFullName() }}</h2>
+
+    <!--3.通过computed-->
+    <h2>{{ fullName }}</h2>
+    <h2>{{ fullName }}</h2>
+    <h2>{{ fullName }}</h2>
+    <h2>{{ fullName }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: 'Kobe',
+      lastName: 'Bryant',
+    }
+  },
+  computed: {
+    fullName: function() {
+      console.log('1')
+      return this.firstName + ' ' + this.lastName
+    },
+  },
+  methods: {
+    getFullName: function() {
+      console.log('2')
+      return this.firstName + ' ' + this.lastName
+    },
+  },
+}
+</script>
